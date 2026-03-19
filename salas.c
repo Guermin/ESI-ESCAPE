f22f12ad990b6b1791d672c2d703fbcfd7edc73b
Luiih83 <luis.gomezrosendo@alum.uca.es>
2026-03-19 17:43:30 +0100

    Inicio modulo salas

--- salas.c
+++ salas.c
@@ -1,3 +1,31 @@
 #include "salas.h"
 #include <stdio.h>
 #include <string.h>
+
+
+Sala sala_crear(int id, const char *nombre, const char *descripcion, TipoSala tipo){
+    Sala nueva_sala;
+
+    nueva_sala.id = id;
+    nueva_sala.tipo = tipo; //Asignación de datos
+
+    strcpy(nueva_sala.nombre, nombre);
+    strcpy(nueva_sala.descripcion, descripcion);
+
+    for (int i = 0; i < MAX_OBJETOS_SALA; i++){
+        nueva_sala.id_objetos[i] = -1;
+    }
+
+    for (int i = 0; i < MAX_PUZZLES_SALA; i++){
+        nueva_sala.id_puzzles[i] = -1;
+    }
+
+    return nueva_sala;
+}
+
+void sala_mostrar_info(Sala sala){
+    printf("\n===================================\n");
+    printf("LUGAR: %s\n", sala.nombre);
+    printf("====================================\n");
+    printf("%s\n\n", sala.descripcion);
+}
--- salas.h
+++ salas.h
@@ -1,4 +1,29 @@
 #ifndef SALAS_H
 #define SALAS_H
 
+#define MAX_NOMBRE_SALA 50
+#define MAX_DESC_SALA 500
+#define MAX_OBJETOS_SALA 5
+#define MAX_PUZZLES_SALA 2
+
+typedef enum {
+    INICIAL,
+    NORMAL,
+    SALIDA
+} TipoSala;
+
+typedef struct{
+    int id;
+    char nombre[MAX_NOMBRE_SALA];
+    char descripcion [MAX_DESC_SALA];
+    TipoSala tipo;
+    int id_objetos[MAX_OBJETOS_SALA];
+    int id_puzzles[MAX_PUZZLES_SALA];
+}Sala;
+
+
+Sala sala_crear(int id, const char *nombre, const char *descripcion, TipoSala tipo);
+
+void sala_mostrar_info(Sala sala);
+
 #endif
