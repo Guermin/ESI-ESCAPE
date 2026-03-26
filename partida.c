cd7b860ad52544ff66abe7fe1d8793bf88d1919a
Guermin <germanheca@gmail.com>
2026-03-26 12:02:04 +0100

    Módulo partida creo que entero

--- partida.c
+++ partida.c
@@ -1,4 +1,114 @@
 #include "partida.h"
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+
+
+EstadoPartida partida_crear(const char *nombre_jugador, int id_sala_inicio) {
+    EstadoPartida nueva_partida;
+    nueva_partida.num_salas = 0;
+    nueva_partida.num_conexiones = 0;
+    nueva_partida.num_objetos = 0;
+    nueva_partida.num_puzzles = 0;
+    nueva_partida.jugador_actual = jugador_crear(nombre_jugador, id_sala_inicio);
+    return nueva_partida; 
+}
+
+
+
+void partida_destruir(EstadoPartida *p) {
+    jugador_destruir(&(p->jugador_actual));
+}
+
+int partida_añadir_sala(EstadoPartida *p, Sala s) {
+    int exito = 0;
+    if (p->num_salas < MAX_SALAS_PARTIDA) {
+        p->salas[p->num_salas] = s;
+        p->num_salas = p->num_salas + 1;
+        exito = 1;
+    }
+    return exito;
+}
+
+
+
+int partida_añadir_conexion(EstadoPartida *p, Conexion c) {
+    int exito = 0;
+    if (p->num_conexiones < MAX_CONEXIONES_PARTIDA) {
+        p->conexiones[p->num_conexiones] = c;
+        p->num_conexiones = p->num_conexiones + 1;
+        exito = 1;
+    }
+    return exito;
+}
+
+
+int partida_añadir_objeto(EstadoPartida *p, Objeto obj) {
+    int exito = 0;
+    if (p->num_objetos < MAX_OBJETOS_PARTIDA) {
+        p->objetos[p->num_objetos] = obj;
+        p->num_objetos = p->num_objetos + 1;
+        exito = 1;
+    }
+    return exito;
+}
+
+
+
+int partida_añadir_puzzle(EstadoPartida *p, Puzzle puz) {
+    int exito = 0;
+    if (p->num_puzzles < MAX_PUZZLES_PARTIDA) {
+        p->puzzles[p->num_puzzles] = puz;
+        p->num_puzzles = p->num_puzzles + 1;
+        exito = 1;
+    }
+    return exito;
+}
+
+
+
+Sala* partida_obtener_sala(EstadoPartida *p, int id) {
+    Sala* encontrada = NULL;
+    int i = 0;
+    while (i < p->num_salas && encontrada == NULL) {
+        if (p->salas[i].id == id) encontrada = &(p->salas[i]);
+        i++;
+    }
+    return encontrada;
+}
+
+
+
+
+Conexion* partida_obtener_conexion(EstadoPartida *p, int id) {
+    Conexion* encontrada = NULL;
+    int i = 0;
+    while (i < p->num_conexiones && encontrada == NULL) {
+        if (p->conexiones[i].id == id) encontrada = &(p->conexiones[i]);
+        i++;
+    }
+    return encontrada;
+}
+
+
+
+
+Objeto* partida_obtener_objeto(EstadoPartida *p, int id) {
+    Objeto* encontrado = NULL;
+    int i = 0;
+    while (i < p->num_objetos && encontrado == NULL) {
+        if (p->objetos[i].id == id) encontrado = &(p->objetos[i]);
+        i++;
+    }
+    return encontrado;
+}
+
+
+
+Puzzle* partida_obtener_puzzle(EstadoPartida *p, int id) {
+    Puzzle* encontrado = NULL;
+    int i = 0;
+    while (i < p->num_puzzles && encontrado == NULL) {
+        if (p->puzzles[i].id == id) encontrado = &(p->puzzles[i]);
+        i++;
+    }
+    return encontrado;
+}
--- partida.h
+++ partida.h
@@ -6,7 +6,49 @@
 #include "objetos.h"
 #include "jugadores.h"
 
+#define MAX_SALAS_PARTIDA 6
+#define MAX_CONEXIONES_PARTIDA 12
+#define MAX_OBJETOS_PARTIDA 4
+#define MAX_PUZZLES_PARTIDA 4
 
+typedef struct {
+    Sala salas[MAX_SALAS_PARTIDA];
+    int num_salas;                   
+    
+    Conexion conexiones[MAX_CONEXIONES_PARTIDA];
+    int num_conexiones;
+    
+    Objeto objetos[MAX_OBJETOS_PARTIDA];
+    int num_objetos;
+    
+    Puzzle puzzles[MAX_PUZZLES_PARTIDA];
+    int num_puzzles;
+    
+    Jugador jugador_actual;          
+} EstadoPartida;
+
+/* PRE:  'nombre_jugador' no es NULL. 
+ * POST: Tablero vacío, todos los contadores a 0, jugador creado e inicializado. 
+ */
+EstadoPartida partida_crear(const char *nombre_jugador, int id_sala_inicio);
+
+
+/* PRE:  'p' puntero válido.
+ * POST: Libera los elementos dinámicos que existen en la partida.
+ */
+void partida_destruir(EstadoPartida *p);
+
+/* Funciones de inserción, devuelve 1 en caso de exito, o 0 en caso de que supere el limite establecido en las macros. */
+int partida_añadir_sala(EstadoPartida *p, Sala s);
+int partida_añadir_conexion(EstadoPartida *p, Conexion c);
+int partida_añadir_objeto(EstadoPartida *p, Objeto obj);
+int partida_añadir_puzzle(EstadoPartida *p, Puzzle puz);
+
+/* Funciones de búsqueda, devuelven el puntero en memoria del elemento, o null en caso de que no exista. */
+Sala* partida_obtener_sala(EstadoPartida *p, int id);
+Conexion* partida_obtener_conexion(EstadoPartida *p, int id);
+Objeto* partida_obtener_objeto(EstadoPartida *p, int id);
+Puzzle* partida_obtener_puzzle(EstadoPartida *p, int id);
 
 
 
