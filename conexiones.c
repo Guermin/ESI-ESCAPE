44a44d0209eac9adeff3bfed3a799aff8a85531f
Luiih83 <luis.gomezrosendo@alum.uca.es>
2026-04-09 17:12:04 +0200

    Comienzo de la documentación y avance de conexiones.c

--- conexiones.c
+++ conexiones.c
@@ -16,3 +16,25 @@ Conexion conexion_crear(int id, const char *nombre, int origen, int destino, int
     
     return nueva_conexion; 
 }
+
+int conexion_intentar_abrir_objeto(Conexion *c, int id_objeto) {
+    int exito = 0; 
+    if (c->activa == 1) {
+        exito = 1;
+    } else if (c->id_objeto_necesario != -1 && c->id_objeto_necesario == id_objeto) {
+        c->activa = 1; 
+        exito = 1;
+    }
+    return exito; 
+}
+
+int conexion_intentar_abrir_puzzle(Conexion *c, int id_puzzle) {
+    int exito = 0;
+    if (c->activa == 1) {
+        exito = 1;
+    } else if (c->id_puzzle_necesario != -1 && c->id_puzzle_necesario == id_puzzle) {
+        c->activa = 1; 
+        exito = 1;
+    }
+    return exito; 
+}
